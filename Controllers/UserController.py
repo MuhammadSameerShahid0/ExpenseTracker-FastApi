5427f46c995a295695851150a08365678b65b248
MuhammadSameerShahid0 <muhammadsameershahid0@gmail.com>
2025-10-14 18:11:02 -0400

    test endpoint

--- Controllers/UserController.py
+++ Controllers/UserController.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException, FastAPI
 from sqlalchemy.orm import Session
+from starlette import status
+from Controllers.WebhookController import SECRET_KEY_TRIGGER
 from Factory.AbstractFactory import MySqlServiceFactory
 from Interfaces.IUserService import IUserService
 from Models.Database import get_db
 from OAuthandJWT.JWTToken import verify_jwt
 from Schema.UserSchema import ContactSchema
+from Webhook.pdf_tasks import generate_and_send_monthly_reports
 
 app = FastAPI()
 UserRouter = APIRouter(tags=["User"])
@@ -31,3 +34,13 @@ def contact( request : ContactSchema, services: IUserService = User_Db_DI):
         return services.contact_message(request)
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
+
+@UserRouter.post("/tggrigger-monthly-report")
+def trggigger_monthly_report(secret_key: str):
+    if secret_key != SECRET_KEY_TRIGGER:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Unauthorized: Invalid secret key."
+        )
+    task = generate_and_send_monthly_reports()
+    return {"status": "success", "message": task}
